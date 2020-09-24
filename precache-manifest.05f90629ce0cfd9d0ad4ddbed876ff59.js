b7c2559735fc9b7c2fb66b3a80accb8a09ae4e90
Alfredo Martinez <psialfredomtz@gmail.com>
2020-09-24 15:16:04 -0500

    Updates

---
+++ precache-manifest.05f90629ce0cfd9d0ad4ddbed876ff59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30af848cebde7040cdbd86c30f6b746e",
+    "url": "/foro-test/index.html"
+  },
+  {
+    "revision": "0acb2ce4362973839387",
+    "url": "/foro-test/static/js/2.b3e59167.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/foro-test/static/js/2.b3e59167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8dbcc7686e860d74c9a",
+    "url": "/foro-test/static/js/main.b601ab51.chunk.js"
+  },
+  {
+    "revision": "e143eab2123d1b7dc534",
+    "url": "/foro-test/static/js/runtime-main.0f16a268.js"
+  },
+  {
+    "revision": "d85a0be92bbce44a74965b2f6684565c",
+    "url": "/foro-test/static/media/404Error-bro.d85a0be9.svg"
+  }
+]);
